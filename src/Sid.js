0df4e8a5be7c508139fec08a159e3a9fd6d921ab
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-09-26 12:42:46 +0530

    fix

--- src/Sid.js
+++ src/Sid.js
@@ -1,3 +1,33 @@
-export default function counter() {
-  return <div>Hello Siddharth</div>;
+import { useState } from "react";
+
+export default function Counter() {
+  const [number, setnumber] = useState(0);
+
+  function handleAdd() {
+    setnumber((number) => number + 1);
+  }
+
+  function handleSub() {
+    setnumber((number) => number - 1);
+  }
+
+  function handlereset() {
+    if (number === 0) return <p>At level zero</p>;
+    setnumber(0);
+  }
+
+  return (
+    <div>
+      {number < 1 ? `At level zero` : `${number}`}
+      <button className="adds" onClick={handleAdd}>
+        Add
+      </button>
+      <button className="subs" onClick={handleSub}>
+        Subtract
+      </button>
+      <button className="subs" onClick={handlereset}>
+        Reset
+      </button>
+    </div>
+  );
 }
