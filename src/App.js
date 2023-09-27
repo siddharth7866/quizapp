741aed08029990c5dbabc02ace448a42fab6466f
siddharth vishen <42781456+siddharth7866@users.noreply.github.com>
2023-09-27 13:53:05 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
-
+const initialState = { questions: [], status: "loading" };
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRcvd":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "Error":
+      return { ...state, status: "Data not loaded" };
+    default:
+      throw new Error("Action unknown ");
+  }
+}
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRcvd", payload: data }))
+      .catch((err) => dispatch({ type: "Error" }));
+  }, []);
   return (
     <div>
       <Header />
